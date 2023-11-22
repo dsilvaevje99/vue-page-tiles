987bca4f7e432246e81a2548c790c9ab57cba1e4
Daniel Silva <daniel.silva@cegal.com>
2023-11-22 07:08:54 +0100

    Export all components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,41 @@
+import type { PageContent, LocaleConfig } from "./interfaces";
+
 import PageTiles from "./components/PageTiles.vue";
 import PageTileEditor from "./components/PageTileEditor.vue";
 
-export { PageTiles, PageTileEditor };
+import Heading from "./components/display/text/Heading.vue";
+import EditableHeading from "./components/edit/text/EditableHeading.vue";
+
+import Paragraph from "./components/display/text/Paragraph.vue";
+import EditableParagraph from "./components/edit/text/EditableParagraph.vue";
+
+import VerticalSpacer from "./components/display/layout/VerticalSpacer.vue";
+import EditableVerticalSpacer from "./components/edit/layout/EditableVerticalSpacer.vue";
+
+import Columns from "./components/display/layout/Columns.vue";
+import EditableColumns from "./components/edit/layout/EditableColumns.vue";
+
+import Banner from "./components/display/text/Banner.vue";
+import EditableBanner from "./components/edit/text/EditableBanner.vue";
+
+import Accordion from "./components/display/text/Accordion.vue";
+import EditableAccordion from "./components/edit/text/EditableAccordion.vue";
+
+export {
+  PageContent,
+  LocaleConfig,
+  PageTiles,
+  PageTileEditor,
+  Heading,
+  EditableHeading,
+  Paragraph,
+  EditableParagraph,
+  VerticalSpacer,
+  EditableVerticalSpacer,
+  Columns,
+  EditableColumns,
+  Banner,
+  EditableBanner,
+  Accordion,
+  EditableAccordion,
+};
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,43 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+import Heading from "./components/display/text/Heading.vue";
+import EditableHeading from "./components/edit/text/EditableHeading.vue";
+
+import Paragraph from "./components/display/text/Paragraph.vue";
+import EditableParagraph from "./components/edit/text/EditableParagraph.vue";
+
+import VerticalSpacer from "./components/display/layout/VerticalSpacer.vue";
+import EditableVerticalSpacer from "./components/edit/layout/EditableVerticalSpacer.vue";
+
+import Columns from "./components/display/layout/Columns.vue";
+import EditableColumns from "./components/edit/layout/EditableColumns.vue";
+
+import Banner from "./components/display/text/Banner.vue";
+import EditableBanner from "./components/edit/text/EditableBanner.vue";
+
+import Accordion from "./components/display/text/Accordion.vue";
+import EditableAccordion from "./components/edit/text/EditableAccordion.vue";
+
+const components = {
+  Heading,
+  EditableHeading,
+  Paragraph,
+  EditableParagraph,
+  VerticalSpacer,
+  EditableVerticalSpacer,
+  Columns,
+  EditableColumns,
+  Banner,
+  EditableBanner,
+  Accordion,
+  EditableAccordion,
+};
+
 const app = createApp(App);
+
+Object.entries(components).forEach(([name, component]) => {
+  app.component(name, component);
+});
 
 app.mount("#app");
