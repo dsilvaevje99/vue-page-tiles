3deb0ee016fff850d4db7c2bfa49d0500982dc62
Daniel Silva <daniel.silva@cegal.com>
2023-11-30 08:20:19 +0100

    Added accordion w. overflow-guard on sm screens

--- src/components/display/text/Banner.vue
+++ src/components/display/text/Banner.vue
@@ -1,6 +1,32 @@
 <template>
+  <div class="md:hidden">
+    <Disclosure v-slot="{ open }">
+      <DisclosureButton
+        :class="`flex justify-between w-full  min-w-0 text-left p-4 rounded-md ${
+          bannerType === 'info' ? 'bg-gray-300' : 'bg-yellow-200'
+        }`"
+      >
+        <InformationCircleIcon
+          v-if="bannerType === 'info'"
+          class="h-6 w-6 shrink-0"
+        />
+        <ExclamationTriangleIcon v-else class="h-6 w-6 shrink-0" />
+        <div
+          v-if="tinymceEnabled"
+          :class="`ml-4 flex-1 ${open ? '' : 'line-clamp-1'}`"
+          v-html="locale ? (data?.text as TextLocale)[locale] : data?.text"
+        ></div>
+        <p v-else :class="`ml-4 flex-1 ${open ? '' : 'line-clamp-1'}`">
+          {{ locale ? (data?.text as TextLocale)[locale] : data?.text }}
+        </p>
+        <ChevronUpIcon
+          :class="`shrink-0 h-6 w-6 ml-4 ${open ? 'rotate-180 transform' : ''}`"
+        />
+      </DisclosureButton>
+    </Disclosure>
+  </div>
   <div
-    :class="`flex p-4 rounded-md ${
+    :class="`hidden md:flex p-4 rounded-md ${
       bannerType === 'info' ? 'bg-gray-300' : 'bg-yellow-200'
     }`"
   >
@@ -19,6 +45,8 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { Disclosure, DisclosureButton } from "@headlessui/vue";
+import { ChevronUpIcon } from "@heroicons/vue/20/solid";
 import { Attributes, Data, TextLocale } from "../../../interfaces";
 import {
   InformationCircleIcon,
@@ -46,4 +74,13 @@ const props = defineProps({
 });
 
 const bannerType = computed(() => props.attrs.bannerType || "info");
+/* const accordionProps = computed(() => {
+  return {
+    ...props,
+    data: {
+      text: "",
+      secondaryText: "",
+    }
+  }
+}) */
 </script>
